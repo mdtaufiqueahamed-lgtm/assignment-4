5396441897ba3b4aaff0b32a9557588c7c319715
mdtaufiqueahamed-lgtm <mdtaufiqueahamed@gmail.com>
2026-02-26 15:15:09 +0600

    something added

--- script.js
+++ script.js
@@ -1,175 +1,175 @@
 
 
-// let interviewList = []
-// let rejectedList = []
+let interviewList = []
+let rejectedList = []
 
-// let total = document.getElementById("total")
-// let interviewCount = document.getElementById("interviewCount")
-// let rejectedCount = document.getElementById("rejectedCount")
+let total = document.getElementById("total")
+let interviewCount = document.getElementById("interviewCount")
+let rejectedCount = document.getElementById("rejectedCount")
 
-// let allCardSection = document.getElementById("all-card")
-// let filterSection = document.getElementById("filter-section")
-// const emptyMessage = document.getElementById("empty-message")
+let allCardSection = document.getElementById("all-card")
+let filterSection = document.getElementById("filter-section")
+const emptyMessage = document.getElementById("empty-message")
 
-// let main = document.getElementById("all-card")
-
-
-
-// function calculateCount() {
-
-//     let cards = allCardSection.querySelectorAll(".w-\\[80\\%\\]")
-
-//     total.innerText = cards.length
-//     interviewCount.innerText = interviewList.length
-//     rejectedCount.innerText = rejectedList.length
-// }
-
-// calculateCount()
-
-
-// function toggleStyle(id) {
-
-//     document.getElementById("all-filter-btn").classList.remove("bg-blue-400")
-//     document.getElementById("interview-button").classList.remove("bg-blue-400")
-
-//     document.getElementById("rejected-button").classList.remove("bg-blue-400")
-
-//       document.getElementById("all-filter-btn").classList.add("bg-gray-300")
-//        document.getElementById("interview-button").classList.add("bg-gray-300")
-//      document.getElementById("rejected-button").classList.add("bg-gray-300")
-
-//     document.getElementById(id).classList.remove("bg-gray-300")
-//     document.getElementById(id).classList.add("bg-blue-400")
-
-//     if (id === "all-filter-btn") {
-//         allCardSection.classList.remove("hidden")
-//         filterSection.classList.add("hidden")
-//     }
-
-// if (id === "interview-button") {
-//         showInterview()
-//     }
-
-//     if (id === "rejected-button") {
-//         showRejected()
-//     }
-// }
+let main = document.getElementById("all-card")
 
 
 
+function calculateCount() {
 
-// main.addEventListener("click", function (event) {
+    let cards = allCardSection.querySelectorAll(".w-\\[80\\%\\]")
 
-//     let card = event.target.closest(".w-\\[80\\%\\]")
-//     if (!card) return
+    total.innerText = cards.length
+    interviewCount.innerText = interviewList.length
+    rejectedCount.innerText = rejectedList.length
+}
 
-//     let heading = card.querySelector(".heading").innerText
-//     let details = card.querySelector(".details").innerText
-//     let notes = card.querySelector(".notes").innerText
-//     let statusBtn = card.querySelector(".first-btn")
+calculateCount()
 
-    
-//     if (event.target.classList.contains("interview-btn")) {
 
-//         statusBtn.innerText = "Interview"
+function toggleStyle(id) {
 
-//         let exists = interviewList.find(item => item.heading === heading)
+    document.getElementById("all-filter-btn").classList.remove("bg-blue-400")
+    document.getElementById("interview-button").classList.remove("bg-blue-400")
 
-//         if (!exists) {
-//             interviewList.push({ heading, details, notes })
-//         }
+    document.getElementById("rejected-button").classList.remove("bg-blue-400")
 
-//         rejectedList = rejectedList.filter(item => item.heading !== heading)
+      document.getElementById("all-filter-btn").classList.add("bg-gray-300")
+       document.getElementById("interview-button").classList.add("bg-gray-300")
+     document.getElementById("rejected-button").classList.add("bg-gray-300")
 
-//         calculateCount()
-//     }
+    document.getElementById(id).classList.remove("bg-gray-300")
+    document.getElementById(id).classList.add("bg-blue-400")
+
+    if (id === "all-filter-btn") {
+        allCardSection.classList.remove("hidden")
+        filterSection.classList.add("hidden")
+    }
+
+if (id === "interview-button") {
+        showInterview()
+    }
+
+    if (id === "rejected-button") {
+        showRejected()
+    }
+}
+
+
+
+
+main.addEventListener("click", function (event) {
+
+    let card = event.target.closest(".w-\\[80\\%\\]")
+    if (!card) return
+
+    let heading = card.querySelector(".heading").innerText
+    let details = card.querySelector(".details").innerText
+    let notes = card.querySelector(".notes").innerText
+    let statusBtn = card.querySelector(".first-btn")
 
     
-//     if (event.target.classList.contains("rejected-btn")) {
+    if (event.target.classList.contains("interview-btn")) {
 
-//         statusBtn.innerText = "Rejected"
+        statusBtn.innerText = "Interview"
 
-//         let exists = rejectedList.find(item => item.heading === heading)
+        let exists = interviewList.find(item => item.heading === heading)
 
-//         if (!exists) {
-//             rejectedList.push({ heading, details, notes })
-//         }
+        if (!exists) {
+            interviewList.push({ heading, details, notes })
+        }
 
-//         interviewList = interviewList.filter(item => item.heading !== heading)
+        rejectedList = rejectedList.filter(item => item.heading !== heading)
 
-//         calculateCount()
-//     }
+        calculateCount()
+    }
 
     
-//     if (event.target.innerText === "Delete") {
+    if (event.target.classList.contains("rejected-btn")) {
 
-//         card.remove()
+        statusBtn.innerText = "Rejected"
 
-//         interviewList = interviewList.filter(item => item.heading !== heading)
-//         rejectedList = rejectedList.filter(item => item.heading !== heading)
+        let exists = rejectedList.find(item => item.heading === heading)
 
-//         calculateCount()
-//     }
+        if (!exists) {
+            rejectedList.push({ heading, details, notes })
+        }
 
-// })
+        interviewList = interviewList.filter(item => item.heading !== heading)
 
+        calculateCount()
+    }
 
-// function showInterview() {
-
-//      allCardSection.classList.add("hidden")
-//     filterSection.classList.remove("hidden")
-//     filterSection.innerHTML = ""
-
-//     interviewList.forEach(item => {
-
-//         let div = document.createElement("div")
-//         div.className = "w-[80%] mx-auto bg-base-300 p-6 mb-4"
-
-//         div.innerHTML = `
-//             <h2 class="text-[#002C5C] font-bold">${item.heading}</h2>
-//             <p class="text-sm">${item.details}</p>
-//             <p class="text-sm">${item.notes}</p>
-//             <p class="text-green-600 mt-2">Interview</p>
-//         `
-
-//         filterSection.appendChild(div)
-//     })
-// }
-
-
-// function showRejected() {
-
-//     allCardSection.classList.add("hidden")
-//     filterSection.classList.remove("hidden")
-//     filterSection.innerHTML = ""
-
-//     rejectedList.forEach(item => {
-
-//         let div = document.createElement("div")
-//         div.className = "w-[80%] mx-auto bg-base-300 p-6 mb-4"
-
-//         div.innerHTML = `
-//             <h2 class="text-[#002C5C] font-bold">${item.heading}</h2>
-//             <p class="text-sm">${item.details}</p>
-//             <p class="text-sm">${item.notes}</p>
-//             <p class="text-red-600 mt-2">Rejected</p>
-//         `
-
-//         filterSection.appendChild(div)
-//     })
-// }
-
-// function checkEmpty() {
     
-//     const allCards = allCardSection.querySelectorAll(".w-\\[80\\%\\]")
+    if (event.target.innerText === "Delete") {
+
+        card.remove()
+
+        interviewList = interviewList.filter(item => item.heading !== heading)
+        rejectedList = rejectedList.filter(item => item.heading !== heading)
+
+        calculateCount()
+    }
+
+})
+
+
+function showInterview() {
+
+     allCardSection.classList.add("hidden")
+    filterSection.classList.remove("hidden")
+    filterSection.innerHTML = ""
+
+    interviewList.forEach(item => {
+
+        let div = document.createElement("div")
+        div.className = "w-[80%] mx-auto bg-base-300 p-6 mb-4"
+
+        div.innerHTML = `
+            <h2 class="text-[#002C5C] font-bold">${item.heading}</h2>
+            <p class="text-sm">${item.details}</p>
+            <p class="text-sm">${item.notes}</p>
+            <p class="text-green-600 mt-2">Interview</p>
+        `
+
+        filterSection.appendChild(div)
+    })
+}
+
+
+function showRejected() {
+
+    allCardSection.classList.add("hidden")
+    filterSection.classList.remove("hidden")
+    filterSection.innerHTML = ""
+
+    rejectedList.forEach(item => {
+
+        let div = document.createElement("div")
+        div.className = "w-[80%] mx-auto bg-base-300 p-6 mb-4"
+
+        div.innerHTML = `
+            <h2 class="text-[#002C5C] font-bold">${item.heading}</h2>
+            <p class="text-sm">${item.details}</p>
+            <p class="text-sm">${item.notes}</p>
+            <p class="text-red-600 mt-2">Rejected</p>
+        `
+
+        filterSection.appendChild(div)
+    })
+}
+
+function checkEmpty() {
     
-//     const filterCards = filterSection.querySelectorAll(".w-\\[80\\%\\]")
+    const allCards = allCardSection.querySelectorAll(".w-\\[80\\%\\]")
+    
+    const filterCards = filterSection.querySelectorAll(".w-\\[80\\%\\]")
 
-//     if (allCards.length === 0 && filterCards.length === 0) {
-//         emptyMessage.classList.remove("hidden")
-//     } else {
-//         emptyMessage.classList.add("hidden")
-//     }
-// }
+    if (allCards.length === 0 && filterCards.length === 0) {
+        emptyMessage.classList.remove("hidden")
+    } else {
+        emptyMessage.classList.add("hidden")
+    }
+}
 
-// checkEmpty()
+checkEmpty()
